f7ba0cee381b5b4bfcf44fc8d02604508a463462
liangsen2 <liangsende1@dingtalk.com>
2020-07-28 18:40:04 +0800

    Initial Commit

---
+++ 5.TestViewAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  5.TestViewAnimation
+//
+//  Created by apple on 2020/7/28.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 5.TestViewAnimationTests/__TestViewAnimationTests.swift
@@ -0,0 +1,34 @@
+//
+//  __TestViewAnimationTests.swift
+//  5.TestViewAnimationTests
+//
+//  Created by apple on 2020/7/28.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import XCTest
+@testable import __TestViewAnimation
+
+class __TestViewAnimationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
